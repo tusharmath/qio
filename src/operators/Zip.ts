6af1f643a141743302033f9bd732ca4b54c10507
Tushar Mathur <tusharmath@gmail.com>
2019-05-05 20:23:13 +0530

    refactor(zip): rename type params

--- src/operators/Zip.ts
+++ src/operators/Zip.ts
@@ -11,16 +11,16 @@ export type OR<A, B> = A & B extends never ? never : [A, B]
 /**
  * @ignore
  */
-export class Zip<R1, R2, E1, E2, A, B>
-  implements IFIO<R1 & R2, E1 | E2, OR<A, B>> {
+export class Zip<R1, R2, E1, E2, A1, A2>
+  implements IFIO<R1 & R2, E1 | E2, OR<A1, A2>> {
   public constructor(
-    private readonly a: IFIO<R1, E1, A>,
-    private readonly b: IFIO<R2, E2, B>
+    private readonly a: IFIO<R1, E1, A1>,
+    private readonly b: IFIO<R2, E2, A2>
   ) {}
 
-  public fork(env: R1 & R2, rej: CB<E1 | E2>, res: CB<OR<A, B>>): Cancel {
-    let responseA: A
-    let responseB: B
+  public fork(env: R1 & R2, rej: CB<E1 | E2>, res: CB<OR<A1, A2>>): Cancel {
+    let responseA: A1
+    let responseB: A2
     let count = 0
     const cancel = new Array<Cancel>()
     const onError = (cancelID: number) => (err: E1 | E2) => {
@@ -31,7 +31,7 @@ export class Zip<R1, R2, E1, E2, A, B>
     const onSuccess = () => {
       count += 1
       if (count === 2) {
-        res([responseA, responseB] as OR<A, B>)
+        res([responseA, responseB] as OR<A1, A2>)
       }
     }
 
