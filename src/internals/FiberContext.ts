27d8713cc792ce06b7b350ebe7e32037923703a4
Tushar Mathur <tusharmath@gmail.com>
2019-06-01 17:32:56 +0530

    refactor(fiber): Fiber context can take optional context params

--- src/internals/FiberContext.ts
+++ src/internals/FiberContext.ts
@@ -10,13 +10,13 @@ import {CancellationList} from './CancellationList'
 import {CB} from './CB'
 
 export class FiberContext<E, A> {
-  public readonly cancellationList = new CancellationList()
-  public readonly stackA: Instruction[] = []
-  public readonly stackE: Array<(e: unknown) => Instruction> = []
   public constructor(
     public readonly env: unknown,
     public readonly rej: CB<E>,
     public readonly res: CB<A>,
-    public readonly sh: IScheduler
+    public readonly sh: IScheduler,
+    public readonly cancellationList: CancellationList = new CancellationList(),
+    public readonly stackA: Instruction[] = [],
+    public readonly stackE: Array<(e: unknown) => Instruction> = []
   ) {}
 }
