fda782eaaf8169aa2f91b7fe7a95fa44aafb77f3
Tushar Mathur <tusharmath@gmail.com>
2019-06-07 21:30:28 +0530

    feat(exit): add type checkers

--- src/main/Exit.ts
+++ src/main/Exit.ts
@@ -12,6 +12,12 @@ export type Exit<E, A> =
 export const Exit = {
   ...Status,
   failure: <E>(e: E): Exit<E, never> => [Status.Failure, e],
+  isFailure: <E, A>(x: Exit<E, A>): x is [Status.Failure, E] =>
+    x[0] === Status.Failure,
+  isPending: <E, A>(x: Exit<E, A>): x is [Status.Pending] =>
+    x[0] === Status.Pending,
+  isSuccess: <E, A>(x: Exit<E, A>): x is [Status.Success, A] =>
+    x[0] === Status.Success,
   pending: (): Exit<never, never> => [Status.Pending],
   success: <A>(a: A): Exit<never, A> => [Status.Success, a]
 }
