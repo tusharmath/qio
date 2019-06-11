cbb6650d1efb44706abff2fa828b204ac25fc41c
Tushar Mathur <tusharmath@gmail.com>
2019-06-11 20:23:26 +0530

    docs(example): add example

---
+++ example/README.md
@@ -0,0 +1,10 @@
+# FIO Example
+
+Steps to run example —
+
+```bash
+git clone https://github.com/tusharmath/fearless-io.git
+yarn install
+yarn tsc
+yarn run example
+```
---
+++ example/src/Run.ts
@@ -0,0 +1,29 @@
+/**
+ * Created by tushar on 2019-05-05
+ */
+
+import {prompt} from 'promptly'
+// tslint:disable-next-line: no-import-side-effect
+import 'source-map-support/register'
+
+import {defaultRuntime} from '../../'
+
+import {program} from './Program'
+
+defaultRuntime().execute(
+  program.provide({
+    console: {
+      getStrLn: prompt,
+      // tslint:disable-next-line:no-console
+      putStrLn: (message: string) => console.log(message)
+    },
+    random: {
+      random: () => Math.random()
+    },
+    system: {
+      exit(code: number): void {
+        process.exit(code)
+      }
+    }
+  })
+)
