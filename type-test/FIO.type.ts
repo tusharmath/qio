75a9b6939522b16c3182500cec7839c0788b1518
Tushar Mathur <tusharmath@gmail.com>
2019-06-18 12:41:29 +0530

    test(typings): update test for typings

--- type-test/FIO.type.ts
+++ type-test/FIO.type.ts
@@ -6,51 +6,51 @@ import {FIO} from '../'
 
 //#region ASYNC
 // $ExpectType FIO<never, never, never>
-FIO.async((env1, rej, res) => res(10))
+FIO.async((rej, res) => res(10))
 
 // $ExpectType FIO<never, never, never>
-FIO.async((env: {a: number}, rej, res, runtime) => {
+FIO.async((rej, res, runtime) => {
   const cancel = runtime.delay(() => {}, 10)
 
-  return () => cancel.cancel()
+  return {cancel: () => cancel.cancel()}
 })
 
 // $ExpectType FIO<never, never, never>
-FIO.async((env, rej, res) => res(10))
+FIO.async((rej, res) => res(10))
 
-// $ExpectType FIO<{ a: number; }, never, string>
-FIO.async<{a: number}, never, string>((env, rej, res) => res(10))
+// $ExpectType FIO<never, string, never>
+FIO.async<never, string>((rej, res) => res(10))
 //#endregion
 
 //#region Operators
-// $ExpectType FIO<never, never, number>
+// $ExpectType FIO<never, number, never>
 FIO.of(1000)
 
-// $ExpectType FIO<never, number, never>
+// $ExpectType FIO<number, never, never>
 FIO.reject(1000)
 
-// $ExpectType FIO<never, never, number>
+// $ExpectType FIO<never, number, never>
 FIO.reject(1000).catch(() => FIO.of(10))
 
-// $ExpectType FIO<never, never, number>
+// $ExpectType FIO<never, number, never>
 FIO.encase((a: string, b: number) => parseInt(a, 10) + b)('10', 2)
 
-// $ExpectType FIO<never, never, number>
+// $ExpectType FIO<never, number, never>
 FIO.reject(new Error('!!!')).catch(() => FIO.of(10))
 
-// $ExpectType FIO<never, never, string | number>
+// $ExpectType FIO<never, string | number, never>
 FIO.of('OLA').catch(() => FIO.of(10))
 
-// $ExpectType FIO<never, never, number>
+// $ExpectType FIO<never, number, never>
 FIO.never().catch(() => FIO.of(10))
 
-// $ExpectType FIO<never, never, number>
+// $ExpectType FIO<never, number, never>
 FIO.of(100).catch(() => FIO.never())
 
-// $ExpectType FIO<never, never, string | number>
+// $ExpectType FIO<never, string | number, never>
 FIO.of(1000).catch(() => FIO.of('HI'))
 
-// $ExpectType FIO<never, never, number>
+// $ExpectType FIO<never, number, never>
 FIO.of(10).chain(_ => FIO.of(_))
 //#endregion
 
@@ -61,10 +61,10 @@ FIO.never().zip(FIO.of(10))
 // $ExpectType FIO<never, never, never>
 FIO.never().zip(FIO.never())
 
-// $ExpectType FIO<never, never, [number, Date]>
+// $ExpectType FIO<never, [number, Date], never>
 FIO.of(1000).zip(FIO.of(new Date()))
 
-// $ExpectType FIO<never, never, [number, Date]>
+// $ExpectType FIO<never, [number, Date], never>
 FIO.of(1000).zip(FIO.of(new Date()))
 //#endregion
 
@@ -76,26 +76,9 @@ interface IE2 {
   e: 'e2'
 }
 
-declare const a: FIO<{console: Console}, IE1, number>
-declare const b: FIO<{process: NodeJS.Process}, IE2, string>
+declare const a: FIO<IE1, number, {console: Console}>
+declare const b: FIO<IE2, string, {process: NodeJS.Process}>
 
-// $ExpectType FIO<{ console: Console; } & { process: Process; }, IE1 | IE2, string>
+// $ExpectType FIO<IE1 | IE2, string, { console: Console; } & { process: Process; }>
 a.chain(() => b)
-//#endregion
-
-//#region Race
-// // $ExpectType FIO<unknown, never, never>
-// FIO.never().race(FIO.never())
-
-// // $ExpectType FIO<unknown, never, number>
-// FIO.never().race(FIO.of(10))
-
-// // $ExpectType FIO<unknown, never, number>
-// FIO.never().race(FIO.of(10))
-
-// // $ExpectType FIO<unknown, never, number | Date>
-// FIO.of(1000).race(FIO.of(new Date()))
-
-// // $ExpectType FIO<unknown, number, never>
-// FIO.never().race(FIO.reject(10))
 //#endregion
