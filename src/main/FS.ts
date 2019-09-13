d6f26178754b555a2e1149b55dbb8edc931ba93b
Tushar Mathur <tusharmath@gmail.com>
2019-09-13 11:16:51 +0530

    refactor(fs): add basic fs utils

---
+++ src/main/FS.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by tushar on 13/09/19
+ */
+
+import {promises} from 'fs'
+
+import {FIO} from './FIO'
+
+export const read = FIO.encaseP(promises.read)
+export const readFile = FIO.encaseP(promises.readFile)
+export const write = FIO.encaseP(promises.write)
+export const writeFile = FIO.encaseP(promises.writeFile)
