47ebb6ff8c22f4bce8f0ca38ff13a2316b8f6c97
Tushar Mathur <tusharmath@gmail.com>
2019-09-16 07:49:23 +0530

    refactor(evaluate): move logic to FiberContext

--- src/internals/FiberContext.ts
+++ src/internals/FiberContext.ts
@@ -2,18 +2,25 @@
  * Created by tushar on 2019-05-24
  */
 
+/* tslint:disable: no-unbound-method */
+
+import {check} from 'checked-exceptions'
 import {Either} from 'standard-data-structures'
 import {ICancellable, IScheduler} from 'ts-scheduler'
 
 import {Fiber} from '../main/Fiber'
 import {FIO, UIO} from '../main/FIO'
-import {Instruction} from '../main/Instructions'
+import {Instruction, Tag} from '../main/Instructions'
 import {IRuntime} from '../runtimes/IRuntime'
 
 import {CancellationList} from './CancellationList'
 import {CB} from './CB'
-import {Evaluate} from './Evaluate'
 import {Exit} from './Exit'
+
+const InvalidInstruction = check(
+  'InvalidInstruction',
+  (ins: Instruction) => `${Tag[ins.tag]}`
+)
 
 /**
  * @ignore
@@ -96,8 +103,7 @@ export class FiberContext<E = never, A = never> extends Fiber<E, A>
   public unsafeResume(rej: CB<E>, res: CB<A>): FiberContext<E, A> {
     const id = this.cancellationList.push(
       this.sh.asap(
-        Evaluate,
-        this,
+        this.evaluate.bind(this),
         (cause: E) => {
           this.cancellationList.remove(id)
           rej(cause)
@@ -110,5 +116,118 @@ export class FiberContext<E = never, A = never> extends Fiber<E, A>
     )
 
     return this
+  }
+
+  private evaluate(rej: CB<E>, res: CB<A>): void {
+    let data: unknown
+
+    while (true) {
+      try {
+        const j = this.stackA.pop()
+
+        if (j === undefined) {
+          return res(data as A)
+        }
+
+        switch (j.tag) {
+          case Tag.Constant:
+            data = j.i0
+            break
+
+          case Tag.Call:
+            this.stackA.push(j.i0(...j.i1))
+            break
+
+          case Tag.Reject:
+            const cause = j.i0 as E
+            const handler = this.stackE.pop()
+            if (handler !== undefined) {
+              this.stackA.push(handler(cause))
+            } else {
+              return rej(cause)
+            }
+            break
+
+          case Tag.Try:
+            data = j.i0(data)
+            break
+
+          case Tag.TryM:
+            this.stackA.push(j.i0(data))
+            break
+
+          case Tag.Map:
+            this.stackA.push(FIO.resume(j.i1).asInstruction)
+            this.stackA.push(j.i0)
+            break
+
+          case Tag.Chain:
+            this.stackA.push(FIO.resumeM(j.i1).asInstruction)
+            this.stackA.push(j.i0)
+            break
+
+          case Tag.Catch:
+            this.stackE.push(j.i1)
+            this.stackA.push(j.i0)
+            break
+
+          case Tag.Never:
+            return
+
+          case Tag.Fork:
+            const nContext = this.unsafeFork(j.i0)
+            this.cancellationList.push(nContext)
+            data = nContext
+            break
+
+          case Tag.Provide:
+            this.stackA.push(
+              FIO.resume(i => {
+                this.stackEnv.pop()
+
+                return i
+              }).asInstruction
+            )
+            this.stackA.push(j.i0)
+            this.stackEnv.push(j.i1)
+            break
+
+          case Tag.Access:
+            const env = this.stackEnv[this.stackEnv.length - 1]
+            data = j.i0(env)
+            break
+
+          case Tag.Runtime:
+            data = this.runtime
+            break
+
+          case Tag.Async:
+            const id = this.cancellationList.push(
+              j.i0(
+                err => {
+                  this.cancellationList.remove(id)
+                  this.stackA.push(FIO.reject(err).asInstruction)
+                  this.unsafeResume(rej, res)
+                },
+                val => {
+                  this.cancellationList.remove(id)
+                  this.stackA.push(FIO.of(val).asInstruction)
+                  this.unsafeResume(rej, res)
+                },
+                this.sh
+              )
+            )
+
+            return
+
+          default:
+            this.stackA.push(
+              FIO.reject(new InvalidInstruction(j)).asInstruction
+            )
+        }
+      } catch (e) {
+        this.stackA.push(FIO.reject(e).asInstruction)
+      }
+    }
   }
 }
