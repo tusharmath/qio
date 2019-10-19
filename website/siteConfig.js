34ceef64706d6f7117a5595a7db00c08d9c8369a
Tushar Mathur <tusharmath@gmail.com>
2019-10-19 23:34:45 +0530

    chore(package): update siteconfig
    remove base url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  // baseUrl: '', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
