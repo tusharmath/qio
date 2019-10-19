5cb65a353481d4b5f9e993e45f90dc26af0eb231
Tushar Mathur <tusharmath@gmail.com>
2019-10-19 23:45:07 +0530

    chore(package): update siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://tusharm.com/fearless-io', // Your website URL
-  baseUrl: '/test-site/', // Base URL for your project */
+  baseUrl: '/fearless-io/test-site/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
