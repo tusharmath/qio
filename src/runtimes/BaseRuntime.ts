7428a1d84556d55f1fd14068f9dcfb049e988edc
Tushar Mathur <tusharmath@gmail.com>
2019-10-19 18:19:49 +0530

    fix(runtime): handle invalid values inside runtime

--- src/runtimes/BaseRuntime.ts
+++ src/runtimes/BaseRuntime.ts
@@ -10,10 +10,14 @@ import {FIO} from '../main/FIO'
 import {IRuntime} from './IRuntime'
 
 export abstract class BaseRuntime implements IRuntime {
+  public readonly maxInstructionCount: number
   public abstract readonly scheduler: IScheduler
-  public constructor(
-    public readonly maxInstructionCount: number = Number.MAX_SAFE_INTEGER
-  ) {}
+  public constructor(maxInstructionCount: number = Number.MAX_SAFE_INTEGER) {
+    this.maxInstructionCount = Math.min(
+      Math.max(1, maxInstructionCount),
+      Number.MAX_SAFE_INTEGER
+    )
+  }
   public abstract setMaxInstructionCount(maxInstructionCount: number): IRuntime
   public unsafeExecute<E, A>(io: FIO<E, A>, cb?: CBOption<E, A>): ICancellable {
     return Fiber.unsafeExecuteWith(io, this, cb)
