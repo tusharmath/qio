2685c1a8776f87c8fecdb4bbcfa4fc723bf42a03
Tushar Mathur <tusharmath@gmail.com>
2019-10-20 07:17:44 +0530

    chore(docs): update site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [
     caption: 'User1',
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
+    image: '/fearless-io/test-site/img/undraw_open_source.svg',
     infoLink: 'https://www.facebook.com',
     pinned: true
   }
