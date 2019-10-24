89adc95499233f169d9c8b1c9f8ad4719920dd07
Tushar Mathur <tusharmath@gmail.com>
2019-10-24 19:10:55 +0530

    refactor(benchmarks): update benchmark generation script
    affects: @qio/benchmarks

--- packages/benchmarks/run.sh
+++ packages/benchmarks/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-FILE_NAME="$1/BENCHMARKS.md"
-echo "# Benchmarks" > "${FILE_NAME}"
-ls -d $1/*.js | xargs -L 1 node >> "${FILE_NAME}"
+cp internals/BENCHMARKS.tmpl.md $2
+
+ls -d $1/*.js | xargs -L 1 node >> $2
