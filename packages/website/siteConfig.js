b1af510d6aa85b8c89483a45a60adbef36bae397
Tushar Mathur <tusharmath@gmail.com>
2019-10-31 17:52:21 +0530

    docs(navigation): update sidebar config

--- packages/website/siteConfig.js
+++ packages/website/siteConfig.js
@@ -45,7 +45,7 @@ const siteConfig = {
     // {doc: 'doc4', label: 'API'},
     // {page: 'help', label: 'Help'},
     {doc: 'fp/introduction', label: 'Getting Started'},
-    {doc: 'core/using-qio', label: 'Usage'},
+    {doc: 'core/installation', label: 'Tutorial'},
     {doc: 'api/globals', label: 'API'},
     {doc: 'others/ecosystem', label: 'Ecosystem'},
     {doc: 'others/benchmarks', label: 'Benchmarks'},
