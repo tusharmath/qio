0c17835392c1c206abdf9fd3ac69440a6fd0bcd1
Tushar Mathur <tusharmath@gmail.com>
2019-11-02 19:24:51 +0530

    chore(script): add change qio signature script

---
+++ scripts/qio-change-signature.ts
@@ -0,0 +1,31 @@
+/* tslint:disable */
+import {Transformation} from 'ts-codemod'
+import * as ts from 'typescript'
+Error.stackTraceLimit = Infinity
+// my-custom-transformation.ts
+export default class QIOChangeSignature extends Transformation {
+  public visit(node: ts.Node): ts.VisitResult<ts.Node> {
+    if (
+      ts.isTypeReferenceNode(node) &&
+      ts.isIdentifier(node.typeName) &&
+      node.typeName.text === 'QIO' &&
+      node.typeArguments !== undefined
+    ) {
+      //
+      if (node.typeArguments.length === 3) {
+        const [E, A, R] = node.typeArguments
+        const nTypeArguments = ts.createNodeArray([A, E, R])
+
+        return ts.createTypeReferenceNode(node.typeName, nTypeArguments)
+      }
+      if (node.typeArguments.length === 2) {
+        const [E, A] = node.typeArguments
+        const nTypeArguments = ts.createNodeArray([A, E])
+
+        return ts.createTypeReferenceNode(node.typeName, nTypeArguments)
+      }
+    }
+
+    return node
+  }
+}
