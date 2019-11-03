5594983b4cbfe9aa897b63d5e95be59900209140
Tushar Mathur <tusharmath@gmail.com>
2019-11-04 02:16:05 +0530

    test(typings): update tests for typings
    affects: @qio/type-test

--- packages/type-test/__tests__/FIO.type.ts
+++ packages/type-test/__tests__/FIO.type.ts
@@ -9,62 +9,58 @@ import {defaultRuntime, QIO} from '@qio/core'
 QIO.asyncIO((rej, res) => res(10))
 
 // $ExpectType QIO<never, never, unknown>
-QIO.asyncIO((rej, res, runtime) => {
-  const cancel = runtime.delay(() => {}, 10)
-
-  return {cancel: () => cancel.cancel()}
-})
+QIO.asyncIO((rej, res, runtime) => runtime.delay(() => {}, 10))
 
 // $ExpectType QIO<never, never, unknown>
 QIO.asyncIO((rej, res) => res(10))
 
-// $ExpectType QIO<never, string, unknown>
-QIO.asyncIO<string>((rej, res) => res(10))
+// $ExpectType QIO<string, never, unknown>
+QIO.asyncIO<string>((rej, res) => ({cancel: () => {}}))
 //#endregion
 
 //#region Operators
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.of(1000)
 
-// $ExpectType QIO<number, never, unknown>
+// $ExpectType QIO<never, number, unknown>
 QIO.reject(1000)
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.reject(1000).catch(() => QIO.of(10))
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.encase((a: string, b: number) => parseInt(a, 10) + b)('10', 2)
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.reject(new Error('!!!')).catch(() => QIO.of(10))
 
-// $ExpectType QIO<never, string | number, unknown>
+// $ExpectType QIO<string | number, never, unknown>
 QIO.of('OLA').catch(() => QIO.of(10))
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.never().catch(() => QIO.of(10))
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.of(100).catch(() => QIO.never())
 
-// $ExpectType QIO<never, string | number, unknown>
+// $ExpectType QIO<string | number, never, unknown>
 QIO.of(1000).catch(() => QIO.of('HI'))
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.of(10).chain(_ => QIO.of(_))
 //#endregion
 
 //#region ZIP
-// $ExpectType QIO<never, [never, number], unknown>
+// $ExpectType QIO<[never, number], never, unknown>
 QIO.never().zip(QIO.of(10))
 
-// $ExpectType QIO<never, [never, never], unknown>
+// $ExpectType QIO<[never, never], never, unknown>
 QIO.never().zip(QIO.never())
 
-// $ExpectType QIO<never, [number, Date], unknown>
+// $ExpectType QIO<[number, Date], never, unknown>
 QIO.of(1000).zip(QIO.of(new Date()))
 
-// $ExpectType QIO<never, [number, Date], unknown>
+// $ExpectType QIO<[number, Date], never, unknown>
 QIO.of(1000).zip(QIO.of(new Date()))
 //#endregion
 
@@ -89,30 +85,30 @@ declare const b: QIO<
     process: NodeJS.Process
   }
 >
-// $ExpectType QIO<IE1 | IE2, string, { console: Console; } & { process: Process; }>
+// $ExpectType QIO<string, IE1 | IE2, { console: Console; } & { process: Process; }>
 a.chain(() => b)
 //#endregion
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.never().map(_ => 10)
 
 // $ExpectType QIO<never, never, unknown>
 QIO.of(10).chain(QIO.never)
 
-// $ExpectType QIO<never, [number, never], unknown>
+// $ExpectType QIO<[number, never], never, unknown>
 QIO.of(10).zip(QIO.never())
 
-// $ExpectType QIO<never, [never, number], unknown>
+// $ExpectType QIO<[never, number], never, unknown>
 QIO.never().zip(QIO.of(10))
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.never().zipWith(QIO.of(10), (a, b) => 10)
 
-// $ExpectType QIO<never, number, unknown>
+// $ExpectType QIO<number, never, unknown>
 QIO.never().zipWithPar(QIO.of(10), (a, b) => 10)
 
-// $ExpectType QIO<never, void, unknown>
+// $ExpectType QIO<void, never, unknown>
 QIO.never().raceWith(QIO.of(10), QIO.void, QIO.void)
 
-// $ExpectError Argument of type 'QIO<never, number, string>' is not assignable to parameter of type 'QIO<never, number, unknown>'.
+// $ExpectError Argument of type 'QIO<number, never, string>' is not assignable to parameter of type 'QIO<number, never, unknown>'.
 defaultRuntime().unsafeExecute(QIO.access((_: string) => _.length))
