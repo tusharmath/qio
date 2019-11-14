df7821e4cc397fb7ccf3b519390802c09db0b394
Tushar Mathur <tusharmath@gmail.com>
2019-11-14 15:45:00 +0530

    refactor(env): rename env from axios to http
    affects: @qio/http
    
    BREAKING CHANGE:
    renaming \`IAxiosEnv\` to \`IHttpEnv\`

---
+++ packages/http/__tests__/Http.test.ts
@@ -0,0 +1,38 @@
+import {QIO, testRuntime} from '@qio/core'
+import {assert} from 'chai'
+
+import {AxiosResponse, httpEnv, request} from '../lib/Http'
+
+describe('http', () => {
+  context('mock', () => {
+    it('should return a response', () => {
+      const TEST_RESPONSE = {
+        data: 'HELLO WORLD',
+        headers: {},
+        status: 200,
+        statusText: 'ok'
+      }
+      const config = {url: 'www.abc.com'}
+      const actual = testRuntime().unsafeExecuteSync(
+        request(config).provide({
+          http: {
+            request: config0 =>
+              QIO.of<AxiosResponse>({...TEST_RESPONSE, config: config0})
+          }
+        })
+      )
+      const expected = {
+        ...TEST_RESPONSE,
+        config
+      }
+
+      assert.deepStrictEqual(actual, expected)
+    })
+  })
+  context('httpEnv', () => {
+    it('should be passable', () => {
+      const config = {url: 'www.abc.com'}
+      testRuntime().unsafeExecute(request(config).provide({http: httpEnv}))
+    })
+  })
+})
--- packages/http/lib/Http.ts
+++ packages/http/lib/Http.ts
@@ -2,16 +2,17 @@
 
 import {QIO} from '@qio/core'
 import Axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios'
+export * from 'axios'
 
 export const request = (config: AxiosRequestConfig) =>
-  QIO.accessM((_: IAxiosEnv) => _.axios.request(config))
+  QIO.accessM((_: IHttpEnv) => _.http.request(config))
 
-export interface IAxiosEnv {
-  axios: {
-    request(config: AxiosRequestConfig): QIO<AxiosResponse, AxiosError>
+export interface IHttpEnv {
+  http: {
+    request(config: AxiosRequestConfig): QIO<AxiosResponse, Error | AxiosError>
   }
 }
 
-export const axiosEnv = {
-  axios: QIO.encaseP(Axios.request)
+export const httpEnv = {
+  request: QIO.encaseP(Axios.request)
 }
