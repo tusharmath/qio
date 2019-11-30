1d37e52a61bbd147a17ee4c4789b350ba1be6cec
Tushar Mathur <tusharmath@gmail.com>
2019-11-30 11:28:51 +0530

    fix(http): handle cancellation via QIO.async API
    affects: @qio/http

--- packages/http/lib/Http.ts
+++ packages/http/lib/Http.ts
@@ -1,18 +1,62 @@
 /* tslint:disable  no-unbound-method  */
 
 import {QIO} from '@qio/core'
-import Axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios'
+import Axios, {
+  AxiosBasicCredentials,
+  AxiosError,
+  AxiosProxyConfig,
+  AxiosResponse,
+  Method,
+  ResponseType
+} from 'axios'
+import * as http from 'http'
+import * as https from 'https'
 export * from 'axios'
 
-export const request = (config: AxiosRequestConfig) =>
+export interface IHttpRequestConfig {
+  auth?: AxiosBasicCredentials
+  data?: unknown
+  headers?: {
+    [k: string]: string
+  }
+  httpAgent?: http.Agent
+  httpsAgent?: https.Agent
+  maxContentLength?: number
+  maxRedirects?: number
+  method?: Method
+  params?: {
+    [k: string]: string | number
+  }
+  proxy?: AxiosProxyConfig | false
+  responseType?: ResponseType
+  url?: string
+  withCredentials?: boolean
+  xsrfCookieName?: string
+  xsrfHeaderName?: string
+}
+
+export const request = (config: IHttpRequestConfig) =>
   QIO.accessM((_: IHttpEnv) => _.http.request(config))
 
 export interface IHttpEnv {
   http: {
-    request(config: AxiosRequestConfig): QIO<AxiosResponse, Error | AxiosError>
+    request(config: IHttpRequestConfig): QIO<AxiosResponse, Error | AxiosError>
   }
 }
 
 export const httpEnv = {
-  request: QIO.encaseP(Axios.request)
+  request: (config: IHttpRequestConfig) =>
+    QIO.asyncIO<AxiosResponse, AxiosError>((res, rej) => {
+      let cancelRequest = () => {}
+      Axios.request({
+        ...config,
+        cancelToken: new Axios.CancelToken(cancel => (cancelRequest = cancel))
+      }).then(res, rej)
+
+      return {
+        cancel: () => {
+          cancelRequest()
+        }
+      }
+    })
 }
