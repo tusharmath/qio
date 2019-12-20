6c044d07a7167a58206024aeaec6247de5b0e760
Tushar Mathur <tusharmath@gmail.com>
2019-12-20 14:17:17 +0530

    feat(package): export \`Exit\` for external use

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:file-name-casing */
 export {Await} from './lib/main/Await'
 export {defaultRuntime} from './lib/runtimes/DefaultRuntime'
+export {Exit} from './lib/main/Exit'
 export {Fiber} from './lib/internals/Fiber'
 export {FiberConfig} from './lib/internals/FiberConfig'
 export {FMap} from './lib/main/FMap'
