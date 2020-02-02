d71a0bee77eca84e722d7d3e21353d0feb89c466
Tushar Mathur <tusharmath@gmail.com>
2020-02-02 17:52:47 +0530

    feat(core): add \`Flag\` type
    affects: @qio/core
    
    \`Flag\` is a more specific form of \`Ref\` and maintains a mutable bit that can be read or updated.

---
+++ packages/core/lib/main/Flag.ts
@@ -0,0 +1,16 @@
+import {QIO} from './QIO'
+import {Ref} from './Ref'
+
+export class Flag {
+  public static of(value: boolean): QIO<Flag> {
+    return Ref.of(value).map(awt => new Flag(awt))
+  }
+  private constructor(private readonly flag: Ref<boolean>) {}
+  public get check(): QIO<boolean> {
+    return this.flag.read
+  }
+
+  public set(value: boolean): QIO<boolean> {
+    return this.flag.set(value)
+  }
+}
